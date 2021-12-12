01a97860d75c43103c6cd68c5ef256f5110b4da1
okiki123 <okikimalomo17@gmail.com>
2021-12-13 00:03:17 +0100

    Updated Commit

---
+++ src/Tours.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours, removeTour}) => {
+  return <section>
+    <div className="title">
+      <h2>Ours tours</h2>
+    </div>
+    <div>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+      })}
+    </div>
+  </section>;
+};
+
+export default Tours;
